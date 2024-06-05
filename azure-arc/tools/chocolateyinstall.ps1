18f0fba2cc585326fdb46afea3a66e777454c253
HealisticEngineer <johnedwardhall@gmail.com>
2024-06-05 18:03:06 +0200

    Linux build fix

--- azure-arc/tools/chocolateyinstall.ps1
+++ azure-arc/tools/chocolateyinstall.ps1
@@ -4,15 +4,14 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = ''
 $checksum = ''
 
-
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  fileType      = 'msi'
-  url           = $url
-  silentArgs    = "/quiet"
-  validExitCodes= @(0, 3010, 1641)
-  checksum      = $checksum
-  checksumType  = 'sha256'
-}
+    packageName   = $env:ChocolateyPackageName
+    fileType      = 'msi'
+    Url64bit = $url
+    silentArgs    = "/quiet"
+    validExitCodes= @(0, 3010, 1641)
+    Checksum64 = $checksum
+    ChecksumType64 = 'sha256'
+  }
 
 Install-ChocolateyPackage @packageArgs
