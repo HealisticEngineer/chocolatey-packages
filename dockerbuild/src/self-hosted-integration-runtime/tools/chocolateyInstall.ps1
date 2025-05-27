21fdad03ee34e5c9f39a2468bc68973df8f25ca1
HealisticEngineer <johnedwardhall@gmail.com>
2025-05-27 08:05:03 +0200

    Registration during package install support

--- dockerbuild/src/self-hosted-integration-runtime/tools/chocolateyInstall.ps1
+++ dockerbuild/src/self-hosted-integration-runtime/tools/chocolateyInstall.ps1
@@ -4,6 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = ''
 $checksum   = ''
 
+$pp = Get-PackageParameters
 
 if ([Version] (Get-CimInstance Win32_OperatingSystem).Version -lt [version] "10.0.0.0") {
   Write-Error "Microsoft Integration Runtime requires a minimum of Windows 10 or Windows Server 2016"
@@ -21,3 +22,14 @@ $packageArgs = @{
 }
 
 Install-ChocolateyPackage @packageArgs
+
+# Register the integration runtime service if key is provided
+if ($pp -and $pp['key']) {
+  # Enable remote access and register the node with the provided key
+  $DmgcmdPath = "C:\Program Files\Microsoft Integration Runtime\5.0\Shared\dmgcmd.exe"
+  $NODE_NAME = $Env:COMPUTERNAME
+  $AUTH_KEY= $($pp['key'])
+  $PORT = "8060"
+  Start-Process $DmgcmdPath -Wait -ArgumentList "-EnableRemoteAccessInContainer", "$($PORT)"
+  Start-Process $DmgcmdPath -Wait -ArgumentList "-RegisterNewNode", $($AUTH_KEY)", "$($NODE_NAME)" 
+}
