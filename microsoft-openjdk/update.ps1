0cf85e0c0781a960bd3407fa8aaaabdcf22e8bb8
HealisticEngineer <johnedwardhall@gmail.com>
2025-12-21 15:47:18 +0100

    Update to LTS

--- microsoft-openjdk/update.ps1
+++ microsoft-openjdk/update.ps1
@@ -21,7 +21,7 @@ function global:au_GetLatest {
    # Send another web request to the specified URL
    $response = Invoke-WebRequest -Uri "https://learn.microsoft.com/en-us/java/openjdk/download"
    # Check if the response content contains a test OpenJDK 21.x.x
-   if($response.content -match "OpenJDK 21\.\d+\.\d+") {
+   if($response.content -match "OpenJDK 25\.\d+\.\d+") {
        # If it does, create a hashtable with the URL and version number
        $Version = ($Matches[0] -split " ")[1]
    } else {
